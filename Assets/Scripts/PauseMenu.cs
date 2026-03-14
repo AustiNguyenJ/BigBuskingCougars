98b895fa025695a0d188ed63f80c95645a56b4bc
Arturo <arturomurga2000@gmail.com>
2026-03-14 03:11:54 -0500

    implemented pause menu with settings&sliders

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -12,22 +13,38 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] private Slider volumeSlider;
     [SerializeField] private Slider vfxSlider;
 
+    private bool isPaused = false;
+
     void Start()
     {
+        if (optionsMenu != null)
+            optionsMenu.SetActive(false);
+
+        if (settingsPanel != null)
+            settingsPanel.SetActive(false);
+
+        Time.timeScale = 1f;
+        isPaused = false;
+
         if (volumeSlider != null)
         {
-            volumeSlider.onValueChanged.AddListener(SetVolume);
             volumeSlider.minValue = 0f;
             volumeSlider.maxValue = 1f;
-            volumeSlider.value = AudioListener.volume;
+
+            float savedVolume = PlayerPrefs.GetFloat("Volume", 1f);
+            volumeSlider.value = savedVolume;
+            AudioListener.volume = savedVolume;
+
+            volumeSlider.onValueChanged.AddListener(SetVolume);
         }
 
         if (vfxSlider != null)
         {
-            vfxSlider.onValueChanged.AddListener(SetVFXIntensity);
             vfxSlider.minValue = 0f;
             vfxSlider.maxValue = 1f;
             vfxSlider.value = 1f;
+
+            vfxSlider.onValueChanged.AddListener(SetVFXIntensity);
         }
     }
 
@@ -35,25 +52,48 @@ public class PauseMenu : MonoBehaviour
     {
         if (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame)
         {
-            optionsMenu.SetActive(!optionsMenu.activeSelf);
-
-            if (!optionsMenu.activeSelf)
-            {
-                settingsPanel.SetActive(false);
-                resumeButton.SetActive(true);
-                settingsButton.SetActive(true);
-                quitButton.SetActive(true);
-            }
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
         }
+    }
+
+    private void PauseGame()
+    {
+        isPaused = true;
+
+        optionsMenu.SetActive(true);
+        settingsPanel.SetActive(false);
+
+        resumeButton.SetActive(true);
+        settingsButton.SetActive(true);
+        quitButton.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(resumeButton);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        Time.timeScale = 0f;
     }
 
     public void ResumeGame()
     {
+        isPaused = false;
+
         optionsMenu.SetActive(false);
         settingsPanel.SetActive(false);
+
         resumeButton.SetActive(true);
         settingsButton.SetActive(true);
         quitButton.SetActive(true);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        Time.timeScale = 1f;
     }
 
     public void OpenSettings()
@@ -61,25 +101,33 @@ public class PauseMenu : MonoBehaviour
         resumeButton.SetActive(false);
         settingsButton.SetActive(false);
         quitButton.SetActive(false);
+
         settingsPanel.SetActive(true);
     }
 
     public void BackToMenu()
     {
         settingsPanel.SetActive(false);
+
         resumeButton.SetActive(true);
         settingsButton.SetActive(true);
         quitButton.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(resumeButton);
     }
 
     public void QuitGame()
     {
+        Time.timeScale = 1f;
         Application.Quit();
     }
 
     public void SetVolume(float volume)
     {
         AudioListener.volume = volume;
+        PlayerPrefs.SetFloat("Volume", volume);
+        Debug.Log("Volume: " + volume);
     }
 
     public void SetVFXIntensity(float intensity)
